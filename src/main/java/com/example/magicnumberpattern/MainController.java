13ca55835328f2dcfaddd1b07573c2a852ff32c2
Kim Levin <levin.kimberly@gmail.com>
2018-01-26 13:31:20 -0500

    Ready for submission!

--- src/main/java/com/example/magicnumberpattern/MainController.java
+++ src/main/java/com/example/magicnumberpattern/MainController.java
@@ -16,15 +16,16 @@ public class MainController {
 
     @RequestMapping("/")
     public String printNums(@RequestParam int number){
+
         StringBuilder rpt = new StringBuilder();
-        String retStr = "";
+        rpt.append("Starting Number: " + Integer.toString(number) + "<br/>");
         int submitNum = 0;
         while ( number > 1) {
             submitNum = (number%2 == 0)?number/2:(number*3 + 1);
             rpt.append(Integer.toString(submitNum) + " ");
             number = submitNum;
         }
-        retStr = rpt.toString();
-        return retStr;
+
+        return rpt.toString();
     }
 }
