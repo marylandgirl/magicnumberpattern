26157c259fbded43a891817b2e2d3fe618e3e3a7
Kim Levin <levin.kimberly@gmail.com>
2018-01-26 13:25:33 -0500

    Algorithm is working!

--- src/main/java/com/example/magicnumberpattern/MainController.java
+++ src/main/java/com/example/magicnumberpattern/MainController.java
@@ -21,8 +21,8 @@ public class MainController {
         int submitNum = 0;
         while ( number > 1) {
             submitNum = (number%2 == 0)?number/2:(number*3 + 1);
-            rpt.append(Integer.toString(submitNum));
-            number = -1;
+            rpt.append(Integer.toString(submitNum) + " ");
+            number = submitNum;
         }
         retStr = rpt.toString();
         return retStr;
